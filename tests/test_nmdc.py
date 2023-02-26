0ecfba94075a1a91a5aae347ab22a87b2137e9a5
Micah Denbraver <macro1@users.noreply.github.com>
2023-02-26 12:33:13 -0800

    remove connection as well. add rudementally chat and fix some encoding

--- tests/test_nmdc.py
+++ tests/test_nmdc.py
@@ -8,9 +8,20 @@ from direct_connect import nmdc
 
 @pytest.mark.asyncio
 async def test_connect(caplog: pytest.LogCaptureFixture) -> None:
+    test_nick = "mcbotter☺️"
+    test_chat = "testing?|&$ ☺️ heh"
+
     caplog.set_level(logging.DEBUG)
-    print("doing test")
-    client = nmdc.NMDC(host="nmdc", socket_timeout=2.0)
+    client = nmdc.NMDC(host="nmdc", nick=test_nick, socket_timeout=2.0)
+
     async with asyncio.timeout(30):  # type: ignore[attr-defined]
         await client.connect()
+        await asyncio.sleep(10)
+        await client.send_message(test_chat)
+        while True:
+            msg = await client.receive_message()
+            print(msg)
+            if msg == f"<{test_nick}> {test_chat}":
+                break
+
     assert client.hub_name == "<Enter hub name here>"
