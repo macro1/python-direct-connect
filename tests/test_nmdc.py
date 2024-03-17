ae6f221567584c24f71e80cf3280050c8b15b2f8
Micah Denbraver <macro@helena.local>
2024-03-17 09:12:08 -0700

    rough draft of missing tests

--- tests/test_nmdc.py
+++ tests/test_nmdc.py
@@ -27,3 +27,50 @@ async def test_connect(caplog: pytest.LogCaptureFixture) -> None:
     await client.wait_closed()
 
     assert client.hub_name == "<Enter hub name here>"
+
+
+@pytest.mark.asyncio
+async def test_connect_with_no_description_comment(caplog: pytest.LogCaptureFixture) -> None:
+    test_nick = "mcbotter☺&️2"
+    test_chat = "testing?|&$ &#36; ☺️ heh"
+
+    caplog.set_level(logging.DEBUG)
+    client = nmdc.NMDC(host="nmdc", nick=test_nick, socket_timeout=2.0)
+    client.description_comment = ''
+
+    await client.connect()
+    await client.send_chat(test_chat)
+    for i in range(10):
+        msg = await asyncio.wait_for(client.get_message(), 1)
+        if msg == {"user": test_nick, "message": test_chat}:
+            break
+    else:
+        pytest.fail("the expected messages were not found")
+
+    client.close()
+    await client.wait_closed()
+
+    assert client.hub_name == "<Enter hub name here>"
+
+@pytest.mark.asyncio
+async def test_connect_with_description_tag(caplog: pytest.LogCaptureFixture) -> None:
+    test_nick = "mcbotter☺&️3"
+    test_chat = "testing?|&$ &#36; ☺️ heh"
+
+    caplog.set_level(logging.DEBUG)
+    client = nmdc.NMDC(host="nmdc", nick=test_nick, socket_timeout=2.0)
+    client.description_tag = 'awesome client v2'
+
+    await client.connect()
+    await client.send_chat(test_chat)
+    for i in range(10):
+        msg = await asyncio.wait_for(client.get_message(), 1)
+        if msg == {"user": test_nick, "message": test_chat}:
+            break
+    else:
+        pytest.fail("the expected messages were not found")
+
+    client.close()
+    await client.wait_closed()
+
+    assert client.hub_name == "<Enter hub name here>"
